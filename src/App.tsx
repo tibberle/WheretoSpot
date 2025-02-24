689fee13e3afc19bae94fb92f3a6e700c72427a0
tibberle <tai29112002@gmail.com>
2025-02-24 17:14:30 +1100

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import GlobalStyles from './styles/GlobalStyles';
-import TextStyles from './styles/text';
-import PageOutline from './styles/PageOutline';
-import BusinessPage from './pages/business';
-import CustomerPage from './pages/customer';
-import AddNewSpotPage from './pages/business/add-a-new-spot';
-import AboutPage from './pages/about';
-import CategoryPage from './pages/customer/category';
-import DahabGuidePage from './pages/customer/dahab-guide';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+import TextStyles from "./styles/text";
+import PageOutline from "./styles/PageOutline";
+import BusinessPage from "./pages/business";
+import CustomerPage from "./pages/customer";
+import AddNewSpotPage from "./pages/business/add-a-new-spot";
+import AboutPage from "./pages/about";
+import CategoryPage from "./pages/customer/category";
+import DahabGuidePage from "./pages/customer/dahab-guide";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <PageOutline/>
+    <Router basename="/WheretoSpot">
+      <PageOutline />
       <GlobalStyles />
       <TextStyles />
-      <Switch>
-        <Route exact path="/" component={BusinessPage} />
-        <Route exact path="/customer" component={CustomerPage} />
-        <Route path="/about-us" component={AboutPage} />
-        <Route path="/business/add-a-new-spot" component={AddNewSpotPage} />
-        <Route path="/customer/category" component={CategoryPage} />
-        <Route path="/customer/dahab-guide" component={DahabGuidePage} />
-      </Switch>
+      <Routes>
+        <Route path="/business" element={<BusinessPage />} />
+        <Route path="/customer" element={<CustomerPage />} />
+        <Route path="/about-us" element={<AboutPage />} />
+        <Route path="/business/add-a-new-spot" element={<AddNewSpotPage />} />
+        <Route path="/customer/category" element={<CategoryPage />} />
+        <Route path="/customer/dahab-guide" element={<DahabGuidePage />} />
+      </Routes>
     </Router>
   );
 };
