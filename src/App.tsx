fb7a806336ab64a4916568390a327dacca2af2d7
tibberle <tai29112002@gmail.com>
2025-02-24 17:51:08 +1100

    deployment 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import GlobalStyles from "./styles/GlobalStyles";
-import TextStyles from "./styles/text";
-import PageOutline from "./styles/PageOutline";
-import BusinessPage from "./pages/business";
-import CustomerPage from "./pages/customer";
-import AddNewSpotPage from "./pages/business/add-a-new-spot";
-import AboutPage from "./pages/about";
-import CategoryPage from "./pages/customer/category";
-import DahabGuidePage from "./pages/customer/dahab-guide";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GlobalStyles from './styles/GlobalStyles';
+import TextStyles from './styles/text';
+import PageOutline from './styles/PageOutline';
+import BusinessPage from './pages/business';
+import CustomerPage from './pages/customer';
+import AddNewSpotPage from './pages/business/add-a-new-spot';
+import AboutPage from './pages/about';
+import CategoryPage from './pages/customer/category';
+import DahabGuidePage from './pages/customer/dahab-guide';
 
 const App: React.FC = () => {
   return (
-    <Router basename="/WheretoSpot">
+    <Router basename="/WheretoSpot/">
       <PageOutline />
       <GlobalStyles />
       <TextStyles />
